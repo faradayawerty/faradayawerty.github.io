5cecda6f41e1bcc35203cdbafb4fcbbd6235c384
faradayawerty <faradayawerty@gmail.com>
2024-10-08 20:40:31 +0300

    -

--- games/zombie/input.js
+++ games/zombie/input.js
@@ -1,9 +1,10 @@
 
 function getInput() {
-  return {
-    keys: {},
-    mouse: {}
-  };
+	return {
+		keys: {},
+		mouse: {},
+		touch: {}
+	};
 }
 
 function keyHandler(keys, e) {
@@ -16,24 +17,46 @@ function mouseHandler(mouse, ctx, e) {
 	mouse.leftButtonPressed = e.buttons === 1 ? true : false;
 }
 
+function touchHandler(touch, ctx, e) {
+	e.preventDefault();
+	touch.exists = e.touches.length > 0 ? true : false;
+	touch.x = (e.touches[0].clientX - ctx.canvas.offsetLeft) * ctx.canvas.width / ctx.canvas.clientWidth;
+	touch.y = (e.touches[0].clientY - ctx.canvas.offsetTop) * ctx.canvas.height / ctx.canvas.clientHeight;
+}
+
+function initializeTouchInput(touch, ctx) {
+	window.addEventListener('touchstart', function(e) {
+		touchHandler(touch, ctx, e);
+	});
+	window.addEventListener('touchmove', function(e) {
+		touchHandler(touch, ctx, e);
+	});
+	window.addEventListener('touchcancel', function(e) {
+		touchHandler(touch, ctx, e);
+	});
+	window.addEventListener('touchend', function(e) {
+		touchHandler(touch, ctx, e);
+	});
+}
+
 function initializeKeyboardInput(keys) {
-  window.addEventListener('keydown', function(e) {
-    keyHandler(keys, e);
-    });
-  window.addEventListener('keyup', function(e) {
-    keyHandler(keys, e);
-    });
+	window.addEventListener('keydown', function(e) {
+		keyHandler(keys, e);
+	});
+	window.addEventListener('keyup', function(e) {
+		keyHandler(keys, e);
+	});
 }
 
 function initializeMouseInput(mouse, ctx) {
-  window.addEventListener('mousemove', function(e) {
-    mouseHandler(mouse, ctx, e);
-    });
-  window.addEventListener('mousedown', function(e) {
-    mouseHandler(mouse, ctx, e);
-    });
-  window.addEventListener('mouseup', function(e) {
-    mouseHandler(mouse, ctx, e);
-    });
+	window.addEventListener('mousemove', function(e) {
+		mouseHandler(mouse, ctx, e);
+	});
+	window.addEventListener('mousedown', function(e) {
+		mouseHandler(mouse, ctx, e);
+	});
+	window.addEventListener('mouseup', function(e) {
+		mouseHandler(mouse, ctx, e);
+	});
 }
 
