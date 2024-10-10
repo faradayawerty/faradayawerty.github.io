277a10d2b59e9985a817ceab4c29ec61223f0a86
faradayawerty <faradayawerty@gmail.com>
2024-10-10 13:38:15 +0300

    -

--- games/zombie/player.js
+++ games/zombie/player.js
@@ -2,7 +2,7 @@
 function player_create(g, x, y) {
 	let p = {
 		speed: 1,
-		body: Matter.Bodies.rectangle(x, y, 2, 2)
+		body: Matter.Bodies.rectangle(x, y, 2, 2, {inertia: Infinity})
 	};
 	Matter.Composite.add(engine.world, p.body);
 	return game_object_create(g, player_update, player_draw, p);
@@ -10,6 +10,7 @@ function player_create(g, x, y) {
 
 function player_update(g, p, dt) {
 	let vel = Matter.Vector.create(0, 0);
+	vel = Matter.Vector.create(p.speed * g.input.joystick.right.dx, p.speed * g.input.joystick.right.dy);
 	if(g.input.keys['a'])
 		vel = Matter.Vector.add(vel, Matter.Vector.create(-p.speed, 0));
 	if(g.input.keys['d'])
@@ -18,7 +19,6 @@ function player_update(g, p, dt) {
 		vel = Matter.Vector.add(vel, Matter.Vector.create(0, -p.speed));
 	if(g.input.keys['s'])
 		vel = Matter.Vector.add(vel, Matter.Vector.create(0, p.speed));
-	vel = Matter.Vector.create(p.speed * g.input.joystick.right.dx, p.speed * g.input.joystick.right.dy);
 	Matter.Body.setVelocity(p.body, vel);
 }
 
