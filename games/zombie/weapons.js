16387b7e22720ebfeee973fa57b14ec0d9ad6196
faradayawerty <faradayawerty@gmail.com>
2024-10-17 20:46:07 +0300

    -

--- games/zombie/weapons.js
+++ games/zombie/weapons.js
@@ -8,8 +8,8 @@ function weapons_get() {
 		shotgun: function(g, x, y, dx, dy) {
 			for(let i = 0; i < 10 + 10 * Math.random(); i++)
 				bullet_create(g, x, y, dx + 0.5 * (Math.random() - 0.5), dy + 0.5 * (Math.random() - 0.5), 2.5 + Math.random());
-				let audio = new Audio('data/sfx/gunshot.ogg');
-				audio.play();
+				//let audio = new Audio('data/sfx/gunshot.ogg');
+				//audio.play();
 			return 750 // cooldown millis
 		}
 	};
