d27f617df40c84d9e4bc2e99f9bafe1387fe0efb
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2024-12-23 15:39:22 +0300

    Create inventory.js

---
+++ games/survival/inventory.js
@@ -0,0 +1,14 @@
+
+function inventory_create() {
+  let inv = {
+    shown: false,
+    items: []
+  };
+  return inv;
+}
+
+function inventory_update(inv, dt) {
+}
+
+function inventory_draw(inv, ctx) {
+}
