27140bac41a7ce087dc7a7196f3724680a304b23
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2024-12-23 15:51:21 +0300

    Update util.js

--- games/survival/util.js
+++ games/survival/util.js
@@ -15,7 +15,7 @@ function drawText(ctx, x, y, text) {
 	ctx.fillText(text, x + 1, y + 1);
 }
 
-function drawMatterBody(ctx, b, color) {
+function fillMatterBody(ctx, b, color) {
 	ctx.beginPath();
 	let vertices = b.vertices;
 	ctx.moveTo(vertices[0].x, vertices[0].y);
@@ -26,3 +26,14 @@ function drawMatterBody(ctx, b, color) {
 	ctx.fill();
 }
 
+function drawMatterBody(ctx, b, color) {
+	ctx.beginPath();
+	let vertices = b.vertices;
+	ctx.moveTo(vertices[0].x, vertices[0].y);
+	for (let j = 1; j < vertices.length; j += 1)
+		ctx.lineTo(vertices[j].x, vertices[j].y);
+	ctx.lineTo(vertices[0].x, vertices[0].y);
+	ctx.lineWidth = 0.1;
+	ctx.strokeStyle = color;
+	ctx.stroke();
+}
