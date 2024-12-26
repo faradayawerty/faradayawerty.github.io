8ce4a302fe3111fdb212ea9eb9e6d64adae83fcb
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2024-12-26 10:18:48 +0300

    Update game.js

--- games/survival/game.js
+++ games/survival/game.js
@@ -6,9 +6,10 @@ function game_create(input_, engine_) {
 		},
 		level: "0x0",
 		objects: [],
-		camera_target: null,
 		input: input_,
-		engine: engine_
+		engine: engine_,
+		offset_x: 0,
+		offset_y: 0
 	};
 	return g;
 }
@@ -30,50 +31,26 @@ function game_object_create(g, name_, data_, func_update, func_draw) {
 }
 
 function game_update(g, dt) {
-	if(g.camera_target == null) {
-		let target = g.objects.find((obj) => obj.name == "player");
-		if(target)
-		   g.camera_target = target.data;
-	} else {
-		let level_x = Number(g.level.split("x")[0]);
-		let level_y = Number(g.level.split("x")[1]);
-		let Ox = 2500 * level_x;
-		let Oy = 2500 * level_y;
-		if(g.camera_target.body.position.x < Ox)
-			g.camera_target.want_level = (level_x - 1) + "x" + level_y;
-		else if(g.camera_target.body.position.x > Ox + 2500)
-			g.camera_target.want_level = (level_x + 1) + "x" + level_y;
-		if(g.camera_target.body.position.y < Oy)
-			g.camera_target.want_level = level_x + "x" + (level_y - 1);
-		else if(g.camera_target.body.position.y > Oy + 2500)
-			g.camera_target.want_level = level_x + "x" + (level_y + 1);
-		if(g.camera_target.want_level != g.level)
-			levels_set(g, g.camera_target.want_level);
-	}
 	for(let i = 0; i < g.objects.length; i++) {
 		let obj = g.objects[i];
 		obj.update(obj.data, dt);
+		if(obj.name == "player") {
+			if(obj.data.want_level != g.level)
+				levels_set(g, obj.data.want_level);
+		}
 	}
 }
 
 function game_draw(g, ctx) {
 	ctx.save();
-	if(g.camera_target != null) {
-		ctx.translate(ctx.canvas.width / 2, ctx.canvas.height / 2);
-		ctx.translate(-g.camera_target.body.position.x, -g.camera_target.body.position.y);
-	}
+	ctx.translate(ctx.canvas.width / 2 - g.offset_x, ctx.canvas.height / 2 - g.offset_y);
 	for(let i = 0; i < g.objects.length; i++) {
 		let obj = g.objects[i];
 		obj.draw(obj.data, ctx);
 	}
 	ctx.restore();
-	if(g.camera_target != null) {
-		drawText(ctx, 40, 40,
-			Math.round(g.camera_target.body.position.x)
-			+ ":"
-			+ Math.round(g.camera_target.body.position.y));
-		drawText(ctx, 40, 60, g.level);
-	}
+	drawText(ctx, 40, 40, Math.round(g.offset_x) + ":" + Math.round(g.offset_.y));
+	drawText(ctx, 40, 60, g.level);
 }
 
 function game_update_settings(g) {
@@ -85,7 +62,6 @@ function game_update_settings(g) {
 }
 
 function game_destroy_all_objects(g) {
-	g.camera_target = null;
 	Matter.Composite.clear(g.engine.world);
 	g.objects = [];
 }
@@ -95,7 +71,6 @@ function game_destroy_level(g) {
 		if(g.objects[i].name == "wall")
 			Matter.Composite.remove(g.engine.world, g.objects[i].data.body);
 	}
-	g.objects = g.objects.filter(
-		(obj) => (obj.name != "decorative" && obj.name != "wall"));
+	g.objects = g.objects.filter((obj) => (obj.name != "decorative" && obj.name != "wall"));
 }
 
