679516ac8e7a5c0c85d6f2edc5b1c19cccdf6f5d
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2024-12-26 11:12:32 +0300

    Update levels.js

--- games/survival/levels.js
+++ games/survival/levels.js
@@ -2,8 +2,8 @@
 function levels_set(g, level) {
 	g.level = level;
 	game_destroy_level(g);
-	let  = Number(p.want_level.split("x")[0]);
-	let level_y = Number(p.want_level.split("x")[1]);
+	let  = Number(level.split("x")[0]);
+	let level_y = Number(level.split("x")[1]);
 	let Ox = 2500 * level_x;
 	let Oy = 2500 * level_y;
 	if(level == "0x0") {
