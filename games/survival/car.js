fe1bef21d13cf6a4202b1c7c442e461d21a1e1b6
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2024-12-26 15:21:41 +0300

    Update car.js

--- games/survival/car.js
+++ games/survival/car.js
@@ -1,20 +1,19 @@
 
 function car_create(g, x, y) {
   let c = {
-    speed: 15,
-    color: "#dd7722",
-    body: Matter.Bodies.rectangle(x, y, 225, 400, {
+	speed: 15,
+	color: "#dd7722",
+	body: Matter.Bodies.rectangle(x, y, 225, 400, {
 			inertia: Infinity
 		})
-  };
+  	};
 	Matter.Composite.add(g.engine.world, c.body);
 	return game_object_move_to_top(game_object_create(g, "car", c, car_update, car_draw));
 }
 
-function car_update(c, dt) {
-}
+function car_update(c, dt) {}
 
 function car_draw(c, ctx) {
   	fillMatterBody(ctx, c.body, c.color);
-		drawMatterBody(ctx, c.body, "white");
+	drawMatterBody(ctx, c.body, "white");
 }
