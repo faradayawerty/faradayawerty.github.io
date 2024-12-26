33589918609fbdf43edda3dae070867a7e0f6f48
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2024-12-26 15:23:39 +0300

    Update game.js

--- games/survival/game.js
+++ games/survival/game.js
@@ -81,5 +81,5 @@ function game_destroy_level(g) {
 function game_object_move_to_top(g, i) {
 	let obj = g.objects[i];
 	g.objects.splice(g.objects, i);
-	return g.object.push(obj) - 1;
+	return g.objects.push(obj) - 1;
 }
