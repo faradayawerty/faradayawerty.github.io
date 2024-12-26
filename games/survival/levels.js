0526d994a106bd46bcdc3efb4edb2dfd943b66e4
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2024-12-26 11:47:02 +0300

    Update levels.js

--- games/survival/levels.js
+++ games/survival/levels.js
@@ -28,8 +28,12 @@ function levels_set(g, level) {
 		decorative_rectangle_create(g, Ox + 1150, Oy, 200, 2500, "#222222", "#222222");
 		decorative_rectangle_create(g, Ox, Oy, 2500, 2500, "gray", "white");
 	} else {
-		for(let i = 0; i < 16; i++)
-			decorative_tree_create(g, Ox + i * 2500 / 16 + Math.random() * 2500 / 32, Oy + i * 2500 / 16 + Math.random() * 2500 / 32);
+		N = 8;
+		for(let i = 0; i < N; i++)
+			for(let i = 0; j < N; j++)
+				decorative_tree_create(g,
+						       Ox + i * 2500 / N + Math.random() * 1250 / N, 
+						       Oy + i * 2500 / N + Math.random() * 1250 / N);
 		decorative_grass_create(g, Ox + 40, Oy + 40, 2420, 2420);
 		decorative_rectangle_create(g, Ox, Oy, 2500, 2500, "gray", "white");
 	}
