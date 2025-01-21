d20101817a07b2c8ca50e4c7a359ef00ce343ab3
faradayawerty <faradayawerty@gmail.com>
2025-01-21 20:20:31 +0300

    -

--- games/survival/input.js
+++ games/survival/input.js
@@ -8,7 +8,9 @@ function input_create() {
 			leftButtonPressed: false,
 			wheelUp: false,
 			wheelDown: false
-		}
+		},
+		joystick: {},
+		touch: []
 	};
 }
 
@@ -85,3 +87,95 @@ function initializeMouseInput(mouse, ctx) {
 	});
 }
 
+function touchHandler(touch, joystick, ctx, e) {
+	e.preventDefault();
+	while (touch.length > 0)
+		touch.pop();
+	for (let i = 0; i < e.touches.length; i++) {
+		t = {}
+		t.x = (e.touches[i].clientX - ctx.canvas.offsetLeft) * ctx.canvas.width / ctx.canvas.clientWidth;
+		t.y = (e.touches[i].clientY - ctx.canvas.offsetTop) * ctx.canvas.height / ctx.canvas.clientHeight;
+		touch.push(t);
+	}
+
+	let w = ctx.canvas.width;
+	let h = ctx.canvas.height;
+
+	joystick.radius = Math.min(w / 16, h / 16);
+
+	joystick.left.x = w / 6;
+	joystick.left.y = 5 * h / 6;
+	joystick.right.x = 5 * w / 6;
+	joystick.right.y = 5 * h / 6;
+
+	joystick.left.dx = 0;
+	joystick.left.dy = 0;
+	joystick.right.dx = 0;
+	joystick.right.dy = 0;
+
+	for (let i = 0; i < touch.length; i++) {
+		if (touch[i].x < w / 2 && touch[i].y > h / 2) {
+			joystick.left.dx = -joystick.left.x + touch[i].x;
+			joystick.left.dy = -joystick.left.y + touch[i].y;
+			offset = Math.sqrt(joystick.left.dx * joystick.left.dx + joystick.left.dy * joystick.left.dy);
+			if (offset <= 0)
+				continue;
+			joystick.left.dx = joystick.left.dx / offset;
+			joystick.left.dy = joystick.left.dy / offset;
+		}
+		if (touch[i].x > w / 2 && touch[i].y > h / 2) {
+			joystick.right.dx = -joystick.right.x + touch[i].x;
+			joystick.right.dy = -joystick.right.y + touch[i].y;
+			offset = Math.sqrt(joystick.right.dx * joystick.right.dx + joystick.right.dy * joystick.right.dy);
+			if (offset <= 0)
+				continue;
+			joystick.right.dx = joystick.right.dx / offset;
+			joystick.right.dy = joystick.right.dy / offset;
+		}
+	}
+}
+
+function initializeTouchInput(touch, joystick, ctx) {
+
+	joystick.left = {}
+	joystick.right = {}
+
+	joystick.left.dx = 0;
+	joystick.left.dy = 0;
+	joystick.right.dx = 0;
+	joystick.right.dy = 0;
+
+	window.addEventListener('touchstart', function(e) {
+		touchHandler(touch, joystick, ctx, e);
+	});
+	window.addEventListener('touchmove', function(e) {
+		touchHandler(touch, joystick, ctx, e);
+	});
+	window.addEventListener('touchcancel', function(e) {
+		touchHandler(touch, joystick, ctx, e);
+	});
+	window.addEventListener('touchend', function(e) {
+		touchHandler(touch, joystick, ctx, e);
+	});
+}
+
+function touch_drawCircle(ctx, x, y, r, fill_color, stroke_color) {
+	ctx.beginPath();
+	ctx.arc(x, y, r, 0, 2 * Math.PI, false);
+	ctx.fillStyle = fill_color;
+	ctx.fill();
+	ctx.lineWidth = 1;
+	ctx.strokeStyle = stroke_color;
+	ctx.stroke();
+}
+
+function drawJoysticks(ctx, joystick) {
+	if (Math.abs(joystick.left.dx) > 0 || Math.abs(joystick.left.dy) > 0) {
+		touch_drawCircle(ctx, joystick.left.x, joystick.left.y, joystick.radius, 'gray', 'black');
+		touch_drawCircle(ctx, joystick.left.x + joystick.radius * joystick.left.dx, joystick.left.y + joystick.radius * joystick.left.dy, joystick.radius / 4, 'black', 'white');
+	}
+	if (Math.abs(joystick.right.dx) > 0 || Math.abs(joystick.right.dy) > 0) {
+		touch_drawCircle(ctx, joystick.right.x, joystick.right.y, joystick.radius, 'gray', 'black');
+		touch_drawCircle(ctx, joystick.right.x + joystick.radius * joystick.right.dx, joystick.right.y + joystick.radius * joystick.right.dy, joystick.radius / 4, 'black', 'white');
+	}
+}
