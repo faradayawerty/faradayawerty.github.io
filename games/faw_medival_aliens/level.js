cc9e5bf9cbf58415b5a918d019fdb59e9736b6f0
faradayawerty <faradayawerty@gmail.com>
2025-04-21 10:51:35 +0300

    Revert "-"
    This reverts commit af5ccb732eb434c2d1466d6fb201b3e2e267c942.

---
+++ games/faw_medival_aliens/level.js
@@ -0,0 +1,19 @@
+
+function create_level(tilegrid) {
+	return {
+		tilegrid: tilegrid
+	};
+}
+
+
+function drawTilegrid(ctx, tilegrid, player_x, player_y) {
+	const tileSize = 16;
+	const tileColors = {
+		g: "#4CAF50",
+		s: "#F4E285",
+		w: "#2196F3",
+		t: "#9E9E9E",
+	};
+
+}
+
