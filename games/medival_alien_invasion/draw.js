7033113c082963c687c2be530d9bd39fdedc70cf
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2025-05-14 11:06:53 +0300

    Update draw.js

--- games/medival_alien_invasion/draw.js
+++ games/medival_alien_invasion/draw.js
@@ -1,28 +1,44 @@
 
-
 function draw(ctx, g) {
 
 	ctx.fillStyle = "#000011";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 		
-	let plr = g.objects.player;
+	let plr = g.objects.player; // has to have x, y
 	let camera_x = 0.5 * ctx.canvas.width - plr.x - 0.5 * plr.w;
 	let camera_y = 0.5 * ctx.canvas.height - plr.y - 0.5 * plr.h;
 	
 	ctx.save();
 	ctx.translate(camera_x, camera_y);
 	
-	// draw enemies
-	let es = g.objects.enemies;
-	for(let i = 0; i < es.length; i++) {
-		ctx.fillStyle = es[i].color;
-		ctx.fillRect(es[i].x, es[i].y, es[i].w, es[i].h);
-	}
-
-	// draw player
-	ctx.fillStyle = plr.color;
-	ctx.fillRect(plr.x, plr.y, plr.w, plr.h);
-
+	let es = g.objects.ai.enemies;
+	for(let i = 0; i < es.length; i++)
+		draw_type(ctx, es[i]);
+	
+	draw_type(ctx, plr);
+	
 	ctx.restore();
+}
+
+function draw_type(ctx, obj) {
+	if(!obj.type)
+		return;
+	switch(obj.type) {
+		case: TYPE_SPACESHIP:
+			ctx.fillStyle = "gray";
+			ctx.strokeStyle = "white";
+			ctx.fillRect(obj.x, obj.y, obj.w, obj.h);
+			ctx.strokeRect(obj.x, obj.y, obj.w, obj.h);
+			break;
+		case: TYPE_ASTEROID:
+			ctx.fillStyle = "brown";
+			ctx.strokeStyle = "white";
+			ctx.fillRect(obj.x, obj.y, obj.w, obj.h);
+			ctx.strokeRect(obj.x, obj.y, obj.w, obj.h);
+			break;
+		default:
+			return;
+			break;
+	}
 }
 
