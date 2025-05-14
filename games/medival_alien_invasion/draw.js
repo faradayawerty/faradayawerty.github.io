75ce2833f5b7aeba650e1dc3ed2889bb496dfd2b
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2025-05-14 10:10:55 +0300

    Update draw.js

--- games/medival_alien_invasion/draw.js
+++ games/medival_alien_invasion/draw.js
@@ -5,16 +5,16 @@ function draw(ctx, g) {
 	ctx.fillStyle = "#000011";
 	ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-	let plr = g.objects.player;
-	let camera_x = 0.5 * ctx.canvas.width - plr.x - 0.5 * plr.w;
-	let camera_y = 0.5 * ctx.canvas.height - plr.y - 0.5 * plr.h;
-
 	// draw enemies
 	let es = g.objects.enemies;
 	for(let i = 0; i < es.length; i++) {
 		ctx.fillStyle = es[i].color;
 		ctx.fillRect(es[i].x, es[i].y, es[i].w, es[i].h);
 	}
+	
+	let plr = g.objects.player;
+	let camera_x = 0.5 * ctx.canvas.width - plr.x - 0.5 * plr.w;
+	let camera_y = 0.5 * ctx.canvas.height - plr.y - 0.5 * plr.h;
 	
 	ctx.save();
 	ctx.translate(camera_x, camera_y);
