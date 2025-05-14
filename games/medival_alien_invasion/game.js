e5ca78a511312bff2e608f70f769f81504ad3c3f
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2025-05-14 09:58:16 +0300

    Update game.js

--- games/medival_alien_invasion/game.js
+++ games/medival_alien_invasion/game.js
@@ -9,14 +9,22 @@ function create_game() {
 			scene: 0
 		},
 		objects: {
-			player: create_player(100, 100)
+			player: create_player(100, 100),
+			enemies: [
+				create_enemy(50, 200)
+			]
 		}
 	};
 	return game;
 }
 
 function step(dt, g) {
+	let os = g.objects;
 	g.objects.player.color = g.settings.square_color;
 	g.objects.player.x += inputs.walk_dir.x * dt;
 	g.objects.player.y += inputs.walk_dir.y * dt;
+
+	for(let i = 0; i < os.enemies[i]; i++) {
+		os.enemies[i].x += 0.75 * dt;
+	}
 }
