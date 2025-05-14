c3ae9661ba56877dc28eaf8613b790c9c7ac71d4
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2025-05-14 10:53:52 +0300

    Update spaceship.js

--- games/medival_alien_invasion/spaceship.js
+++ games/medival_alien_invasion/spaceship.js
@@ -1,11 +1,12 @@
 
+let TYPE_SPACESHIP = 1;
+
 function create_spaceship(x, y, size) {
 	return {
+		type: TYPE_SPACESHIP,
 		x: x,
 		y: y,
 		w: size,
 		h: size,
-		color_fill: "gray",
-		color_outline: "white"
 	};
 }
