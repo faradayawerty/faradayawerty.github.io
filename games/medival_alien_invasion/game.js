13289195d443fa3cc31feb91ddb1af986cb4ca74
Фарадей <40428645+faradayawerty@users.noreply.github.com>
2025-05-14 10:20:08 +0300

    Update game.js

--- games/medival_alien_invasion/game.js
+++ games/medival_alien_invasion/game.js
@@ -25,8 +25,6 @@ function step(dt, g) {
 	plr.x += inputs.walk_dir.x * dt;
 	plr.y += inputs.walk_dir.y * dt;
 
-	console.log(inputs.walk_dir, plr, g.objects.player);
-
 	let es = g.objects.enemies;
 	for(let i = 0; i < es.length; i++) {
 		es[i].x += 0.025 * dt;
