e3281ebe683b5567502c8ef9ca51c60baf2a6a6c
faradayawerty <faradayawerty@gmail.com>
2025-10-20 11:56:12 +0300

    -

--- minigames/faw_guess_who/main.js
+++ minigames/faw_guess_who/main.js
@@ -1,12 +1,12 @@
 
 function main() {
 	document.title = "Guess Who!";
+	document.head.appendChild(Object.assign(document.createElement('link'), {rel:'icon', href:'icon.png'}));
 	document.body.style.backgroundColor = "#aabbcc";
 	document.body.style.fontFamily = "Arial, sans-serif";
 	document.body.style.margin = "0";
 	document.body.style.padding = "0";
 }
 
-// Запуск после загрузки документа
 window.addEventListener("DOMContentLoaded", main);
 
