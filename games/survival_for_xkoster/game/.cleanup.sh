b9309c3b7b0f80bc25e698635cc30476938d7a36
faradayawerty <faradayawerty@gmail.com>
2026-02-10 17:21:46 +0300

    -

--- games/survival_for_xkoster/game/.cleanup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-sed -i 's/\/\/.*$//' *.js
-
-sed -i -e '/\/\*/,/\*\//d' *.js
-
-js-beautify -r -t --wrap-line-length=80 --max-preserve-newlines=1 --brace-style=end-expand *.js *.html
-
